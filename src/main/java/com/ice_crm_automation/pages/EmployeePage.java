2ba7bc92d87532cea89d228c6cf4ed88288faf9e
tanvi1807 <tanvianandmanoli@gmail.com>
2023-04-13 12:00:39 +0530

    files are uploaded

--- src/main/java/com/ice_crm_automation/pages/EmployeePage.java
+++ src/main/java/com/ice_crm_automation/pages/EmployeePage.java
@@ -1,13 +1,20 @@
 package com.ice_crm_automation.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class EmployeePage {
 
-	 public By employeeMenu = By.xpath("//li[@ref='admin_Employees']//span");
+
+//	public EmployeePage(WebDriver driver) {
+//		this.driver=driver;
+//	}
+	  public By employeeMenu = By.xpath("//li[@ref='admin_Employees']//span");
 	    public By empSubMenu = By.xpath("//ul[@id='admin_Employees']/li[1]/a");
 	    public By allEmpInfo = By.xpath("//tbody[@class='ant-table-tbody']/tr/td[3]");
 	    public By searchBox = By.id("advanced_search_searchTerm");
 	    public By searchButton = By.xpath("//span[text()='Search']");
+
+
 
 }
--- src/test/java/com/ice_crm_automation/loginpage/EmployeeTest.java
+++ src/test/java/com/ice_crm_automation/loginpage/EmployeeTest.java
@@ -11,61 +11,59 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.ice_crm_automation.pages.EmployeePage;
-import com.ice_crm_automation.pages.loginpage;
+import com.ice_crm_automation.pages.Loginpage;
+
 import com.ice_crm_automation.utility.BaseClass;
 import com.ice_crm_automation.utility.PropertyHandling;
 
 public class EmployeeTest extends BaseClass
 {
 
-	    PropertyHandling property;
-	    loginpage loginPage;
-	    EmployeePage employeePage;
- 
-	   EmployeeTest()
-	    {
-	      PageFactory.initElements(driver,this);    	    
-	    }
-	    @BeforeClass
-	    public void beforeClass() {
-	        property = new PropertyHandling();
-	        launchbrowser(property.getProperty("browser"));
-	        driver.get(property.getProperty("icecrmurl"));
-	        loginPage = new loginpage(driver);
-	        employeePage = new EmployeePage();
-	        String username = property.getProperty("username");
-	        String password = property.getProperty("password");
-	        loginPage.login(username, password);
-	    }
+	PropertyHandling property;
+    Loginpage loginPage;
+    EmployeePage employeePage;
 
-	    @Test
-	    public void verifySearchEmp() throws InterruptedException {
-	        click(employeePage.employeeMenu);
-	        click(employeePage.empSubMenu);
-	        waitUntilElementIsClickable(employeePage.searchButton);
-	        List<String> allEmpFNames = new ArrayList<>();
-	        Thread.sleep(5000);
-	        List<WebElement> allEmpDetails = driver.findElements(employeePage.allEmpInfo);
-	        for (WebElement element : allEmpDetails) {
-	            String firstName = element.getText();
-	            allEmpFNames.add(firstName);
-	            System.out.println(firstName);
-	        }
-	        String lastEmpName = allEmpFNames.get(allEmpDetails.size()- 1);
-	        entertext(employeePage.searchBox, lastEmpName);
-	        click(employeePage.searchButton);
-	        Thread.sleep(5000);
-	        List<WebElement> searchEmpDetails = driver.findElements(employeePage.allEmpInfo);
-	        for (WebElement element : searchEmpDetails) {
-	            String firstName = element.getText();
-	            Assert.assertEquals(firstName, lastEmpName);
-	        }
-	    }
+    @BeforeClass
+    public void beforeClass() {
+        property = new PropertyHandling();
+        launchbrowser(property.getProperty("browser"));
+        driver.get(property.getProperty("icecrmurl"));
+         employeePage = new EmployeePage();
+        String username = property.getProperty("username");
+        String password = property.getProperty("password");
+        loginPage = new Loginpage(driver);
 
-//	    @AfterClass
-//	    public void afterClass() {
-//	        driver.close();
-//	    }
+        loginPage.login(username, password);
+    }
+
+    @Test
+    public void verifySearchEmp() throws InterruptedException {
+        click(employeePage.employeeMenu);
+        click(employeePage.empSubMenu);
+        waitUntilElementIsClickable(employeePage.searchButton);
+        List<String> allEmpFNames = new ArrayList<>();
+        Thread.sleep(5000);
+        List<WebElement> allEmpDetails = driver.findElements(employeePage.allEmpInfo);
+        for (WebElement element : allEmpDetails) {
+            String firstName = element.getText();
+            allEmpFNames.add(firstName);
+        }
+        String lastEmpName = allEmpFNames.get(allEmpDetails.size() - 1);
+        entertext(employeePage.searchBox, lastEmpName);
+        click(employeePage.searchButton);
+        Thread.sleep(5000);
+        List<WebElement> searchEmpDetails = driver.findElements(employeePage.allEmpInfo);
+        for (WebElement element : searchEmpDetails) {
+            String firstName = element.getText();
+            Assert.assertEquals(firstName, lastEmpName);
+        }
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.close();
+    }
+
 }
 	
 
